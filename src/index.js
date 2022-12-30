9b119c53b153f60f1855d7a9f99b3d2b9aa9d2f2
Jose Mauricio Granados Muñoz <josgrana1@publicisgroupe.net>
2022-12-30 15:00:03 -0600

    Adding

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <BrowserRouter basename='www.mgm.social'>
+  <BrowserRouter basename='www.mgm.social/fincaFrontEnd/build/'>
     <Provider store={store}>
       <AppRouter />
     </Provider>
